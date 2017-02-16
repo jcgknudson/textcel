406bf2b5e206e8c3952a8bfbd389e3bd78626d82
John <jknudson@umich.edu>
2017-02-16 16:24:29 -0500

    1) added and tested method for calculating ratio of sent to received messages
    2) added explicit 'sender' field in TextAnalyzer class to anchor calculations
    
    3) minor changes

--- tests.py
+++ tests.py
@@ -1,14 +1,15 @@
-from textapp import TextConversationAnalyzer
+from textapp import TextAnalyzer
+from datetime import datetime
 import unittest
 
 #TODO:
 #Come up with a better way to structure these tests rather than separating
 #	test cases by static and instance methods. Additionally, find a better
 #	way to test multiple conversations without having to create a new 
-#	test class. This may need to be changed in TextConversationAnalyzer	 
+#	test class. This may need to be changed in TextAnalyzer	 
 
 ####UNITTEST FWK######
-class TestTextConversationAnalyzerMethods(unittest.TestCase):
+class TestTextAnalyzerMethods(unittest.TestCase):
 	####Unique word count: ~27 (both), 14 (Jack), 16(Niki)
 	####Average sent without response: 
 	####Relative word frequency: Jack [0.33, 0.66] (you vs u), Niki [0.0, 1.0] (you vs u)
@@ -17,8 +18,8 @@ class TestTextConversationAnalyzerMethods(unittest.TestCase):
 	CONVERSATIONS= [ CONVO_1 ]
 
 	def __init__(self, *args, **kwargs):
-		super(TestTextConversationAnalyzerMethods, self).__init__(*args, **kwargs)
-		self.analyzer = TextConversationAnalyzer(conversation_fname = self.CONVO_1)
+		super(TestTextAnalyzerMethods, self).__init__(*args, **kwargs)
+		self.analyzer = TextAnalyzer(conversation_fname = self.CONVO_1, sender = "John Knudson")
 
 	def setUp(self):
 		self.analyzer.reset_conv_file()
@@ -37,9 +38,20 @@ class TestTextConversationAnalyzerMethods(unittest.TestCase):
 	def test_relative_word_frequency(self):
 		expected = {"John Knudson":{"you": 1.0/3.0, "u": 2.0/3.0}, "Niki Waghani": {"you": 1.0, "u": 0.0}}
 		self.assertEqual(self.analyzer.relativeWordFrequency(["u", "you"]), expected)
+
+	def test_ratio_sent_received_no_window(self):
+		expected = 3.0/6.0
+		self.assertEqual(self.analyzer.ratioSentReceived(None), expected)
+
+	def test_ratio_sent_received(self):
+		expected = 2.0/6.0
+		begin_time = datetime(year=1994, month=8, day=17, hour=0, minute=0, second=0)
+		end_time = datetime(year=1994, month=8, day=17, hour=1, minute=40, second=0) 
+		window = (begin_time, end_time)
+		self.assertEqual(self.analyzer.ratioSentReceived(window), expected)
 		
 
-class TestTextConversationAnalyzerStaticMethods(unittest.TestCase):
+class TestTextAnalyzerStaticMethods(unittest.TestCase):
 
 	TEXT_1 = "hello world"
 	TEXT_2 = " "
@@ -53,8 +65,8 @@ class TestTextConversationAnalyzerStaticMethods(unittest.TestCase):
 	TEXTS = [TEXT_1, TEXT_2, TEXT_3, TEXT_4, TEXT_5, TEXT_6, TEXT_7, TEXT_8, TEXT_9]
 
 	def __init__(self, *args, **kwargs):
-		super(TestTextConversationAnalyzerStaticMethods, self).__init__(*args, **kwargs)
-		self.analyzer = TextConversationAnalyzer()
+		super(TestTextAnalyzerStaticMethods, self).__init__(*args, **kwargs)
+		self.analyzer = TextAnalyzer()
 
 	def setUp(self):
 		self.analyzer.reset_conv_file()
--- textapp.py
+++ textapp.py
@@ -2,12 +2,11 @@ import csv
 import string
 from statistics import mode
 from collections import *
-
+from datetime import datetime, timedelta
 
 #TODO: 
 #Come up with some sort of character escaping scheme for including commas in texts
 #	Figure out first how conversations are exported
-#
 #
 #Remove any assumptions about the number of partiipants in each conversation
 #	so that this can also work for group conversations
@@ -18,18 +17,25 @@ from collections import *
 #	Or perhaps a hybrid of the two? Each function may be a static or instance function on a case-by-case basis
 #	What are the pros and cons of each approach?
 #
-#Come up with a cohesive naming scheme for static/util/instance methods
+#Come up with a cohesive naming scheme for static/util/instance methods 
 #
-#Find a better object representation for Analyzer class. It might not make sense to have
-#	only one conversation be analyzed per instance of the class. 
+#Come up with a more intuitive way of organizing functions rather than conversation/etc.
+#
+#Refactor the below class into a "Conversation" object; enable an "Analyzer" class to process
+#	multiple conversation objects
+#
+#Add decent input verification
+#
+#Figure out how dates and times will *actually* be output in the .csv files
 
 #timesMorning = ["0:30", "1:20", "2:00", "3:00", "0:45", "1:25", "2:37", "2:10"]
-class TextConversationAnalyzer(object):
+class TextAnalyzer(object):
 
 	####TRANSLATION#######
+	#This is begging for refactoring
 	IN_PUNC  = ",<.>/?;:\"{[}]|\\!@#$%^&*()-_=+"
 	OUT_PUNC = "                             "
-	TRANSLATOR = str.maketrans(IN_PUNC, OUT_PUNC)
+	_translator = str.maketrans(IN_PUNC, OUT_PUNC)
 	####CONSTANTS#########
 	CSV_TIMESTAMP = 0
 	CSV_SENDER = 1
@@ -39,6 +45,7 @@ class TextConversationAnalyzer(object):
 	####SPECIAL FUNCTIONS#
 	def __init__(self, *args, **kwargs):
 		conv_fname = kwargs.get("conversation_fname", None)
+		sender = kwargs.get("sender", None)
 
 		if(conv_fname):
 			self.conv_file = open(conv_fname)
@@ -46,6 +53,8 @@ class TextConversationAnalyzer(object):
 		else:
 			self.conv_file = None
 			self.reader = None
+
+		self.sender = sender
 
 	def __del__(self):
 		if(self.conv_file is not None):
@@ -62,13 +71,12 @@ class TextConversationAnalyzer(object):
 	def avgWordsPerText(texts):
 		totalWords = 0
 		for text in texts:
-			totalWords += TextConversationAnalyzer.wordCount(text)
-
+			totalWords += TextAnalyzer.wordCount(text)
 		return float(totalWords)/len(texts)
 
 	@staticmethod
 	def wordCount(text):
-		tokens = TextConversationAnalyzer.tokenizeStr(text)
+		tokens = TextAnalyzer.tokenizeStr(text)
 		return len(tokens)
 
 	@staticmethod
@@ -87,10 +95,7 @@ class TextConversationAnalyzer(object):
 
 	#Times 0 to 23 are blocked as follows:
 	#1-5: booty caller, 6-10: early morning, 11-3: midday, 4-9: evening, 10-1: night owl
-	#TODO: Change this fn name
-	def textAnalysis(times):
-		pass
-
+	
 	def amToPm(times):
 		pass
 
@@ -104,9 +109,39 @@ class TextConversationAnalyzer(object):
 		pass
 
 	####CONVERSATION######
-	def ratioSentReceived():
-		pass
+	#window is a tuple of two datetime objects, signifiying the beginning and end of 
+	#the window
+	def ratioSentReceived(self, window):
+		
+		if(self.sender is None):
+			print("wat do")
+			return
 
+		sent = 0
+		received = 0
+		begin_window = datetime.min
+		end_window = datetime.max
+
+		if(window is not None):
+			(begin_window, end_window) = window
+
+		for csv_row in self.reader:
+			if len(csv_row) < self.CSV_DIGEST + 1:
+				continue
+			timestamp = datetime.strptime(csv_row[TextAnalyzer.CSV_TIMESTAMP], "%m/%d/%Y/%H:%M:%S")
+			
+			if(timestamp >= begin_window):
+				if(timestamp <= end_window):	
+					if(self.sender == csv_row[TextAnalyzer.CSV_SENDER]):
+						sent += 1
+					else:
+						received += 1
+				else:
+					#Passed the end of the window 
+					return sent/received if received > 0.0 else 0.0
+		
+		return  sent/received if received > 0.0 else 0.0
+		
 	#	  This could get pretty fancy. Right now our criteria for 'response' is literally just that a text
 	#	was sent from Alice to Bob after Bob sent one to Alice. This isn't quite accurate though. For example,
 	#	we may not wish to consider a text sent from Alice to Bob 3 months after Bob initially texted as a 
@@ -119,13 +154,10 @@ class TextConversationAnalyzer(object):
 	#     I agree it's a good thought. I would go further to say that the pace of texting varies within different conversations between
 	#   the same people. This means we could separate the entire digest into conversations based off of obviously longer breaks between
 	#   subsequent conversations. Then we could apply your method to each conversation block.
-	def avgSentWithoutResponse():
+	def avgSentWithoutResponse(self):
 		pass
 
-	def avgSentWithoutResponseWindow(window):
-		pass
-
-	def uniqueWordCount():
+	def avgSentWithoutResponseWindow(self, window):
 		pass
 
 	#Accepts a list of words to calculate relative percentages
@@ -202,5 +234,5 @@ class TextConversationAnalyzer(object):
 	#	basically our souped up version of str.split()
 	@staticmethod
 	def tokenizeStr(text):
-		replacedCommas = text.translate(TextConversationAnalyzer.TRANSLATOR)
+		replacedCommas = text.translate(TextAnalyzer._translator)
 		return replacedCommas.split()
